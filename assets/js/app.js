6fe69a2eb458f85dc8a9ff15103ad16804d2be28
ivydk <dekk0123@hz.nl>
2021-09-23 11:51:59 +0200

    oefen opdracht 23-9-2021

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,28 @@
-console.log('JavaScript is working!')
+console.log('JavaScript is working!');
+
+// 1. Declaring with variables
+let firstName = "Ivy";
+let lastName = "Dekker";
+let age = "18";
+
+console.log(`Hello, I am ${firstName} ${lastName} and I am ${age} years old`)
+
+// 2. Calculate
+let priceOfAnApple = 0.59;
+let numberOfApples = 200.0;
+let price = priceOfAnApple * numberOfApples;
+console.log(numberOfApples + " apples will cost €" + price)
+
+// 3.  Pythagorean Theorem
+const a = 5;
+const b = 6;
+let c;
+
+c = (a * a) + (b * b)
+
+console.log(c)
+
+// 4. Strings
+let sentence = "Please visit the HZ University of Applied Sciences";
+sentence = sentence.replace("Applied", "Mad");
+console.log(sentence);
