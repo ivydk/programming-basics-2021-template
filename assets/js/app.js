9278b5fb2ff5143267894342baf8748430170f6e
ivydk <dekk0123@hz.nl>
2021-09-23 12:09:03 +0200

    huiswerk opdrachten toegevoegd

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 console.log('JavaScript is working!');
 
+// EXERCISE (SELF PACED)
 // 1. Declaring with variables
 let firstName = "Ivy";
 let lastName = "Dekker";
@@ -26,3 +27,19 @@ console.log(c)
 let sentence = "Please visit the HZ University of Applied Sciences";
 sentence = sentence.replace("Applied", "Mad");
 console.log(sentence);
+
+
+// ASSIGNMENTS (FOR FEEDBACK)
+// 1. JavaScript expression
+let number = 10;
+
+if (number % 2 == 0) {
+    console.log("This is an even number");
+} else {
+    console.log("This isn't an even number");
+}
+
+// 2. String expression
+let sentence2 = "Programming is not so cool";
+sentence2 = sentence2.replace(" not", "");
+console.log(sentence2);
