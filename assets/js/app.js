ad2ec222f6f7baaf508ad597b89d6af0e8d981ba
ivydk <dekk0123@hz.nl>
2021-10-12 08:58:59 +0200

    opdrachten tot 11 okt

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,56 @@
-console.log('JavaScript is working!');
-
-// EXERCISE (SELF PACED)
-// 1. Declaring with variables
-let firstName = "Ivy";
-let lastName = "Dekker";
-let age = "18";
-
-console.log(`Hello, I am ${firstName} ${lastName} and I am ${age} years old`)
-
-// 2. Calculate
-let priceOfAnApple = 0.59;
-let numberOfApples = 200.0;
-let price = priceOfAnApple * numberOfApples;
-console.log(numberOfApples + " apples will cost €" + price)
-
-// 3.  Pythagorean Theorem
-const a = 5;
-const b = 6;
-let c;
-
-c = (a * a) + (b * b)
-
-console.log(c)
-
-// 4. Strings
-let sentence = "Please visit the HZ University of Applied Sciences";
-sentence = sentence.replace("Applied", "Mad");
-console.log(sentence);
+let height = prompt("What's your height?");
+let weight = prompt("What's your weight?");
+let bmi;
 
 
-// ASSIGNMENTS (FOR FEEDBACK)
-// 1. JavaScript expression
-function evenOrOdd(number) {
-    if (number % 2 === 0) {
-        console.log("This is an even number");
-    } else {
-        console.log("This isn't an even number");
-    }
+/** Calculate the BMI
+ * 
+ * @param {Number} weight 
+ * @param {Number} height 
+ * @returns the calculated bmi 
+ */
+function calculateBMI(weight, height) {
+    return Math.round(weight / (height * height));
 }
 
-let number = evenOrOdd(10);
+/** Gives you the classification
+ * 
+ * @param {Number} bmi 
+ * @returns a string that contains your classification
+ */
+function getClassification(bmi) {
+    let classification;
+    if (bmi < 18.5) {
+        classification = "Classification: Under weight";
+    } else if (bmi < 25) {
+        classification = "Classification: Normal weight";
+    } else if (bmi < 30) {
+        classification = "Classification: Slightly over weight";
+    } else {
+        classification = "Classification: Obese";
+    }
+    return classification;
+}
 
-// 2. String expression
-let sentenceProgramming = "Programming is not so cool";
-sentenceProgrammingChanged = sentenceProgramming.replace(" not", "");
-console.log(sentenceProgrammingChanged);
+/** Gives you your sport 
+ * 
+ * @param {Number} bmi 
+ * @returns a string that contains your sport s
+ */
+function getSport(bmi) {
+    let sport;
+    if (bmi < 18.5) {
+        sport = "Sport: Start with personal trainer";
+    } else if (bmi < 25) {
+        sport = "Sport: Start with any programme";
+    } else if (bmi < 30) {
+        sport = "Sport: Start with cardio training";
+    } else {
+        sport = "Sport: Sport: Start with personal trainer";
+    }
+    return sport;
+}
+
+console.log(calculateBMI(weight, height));
+console.log(getClassification(bmi));
+console.log(getSport(bmi));
